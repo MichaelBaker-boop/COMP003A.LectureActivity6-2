6f76896bcd8018b429304e1f8e3270be6ddc2f91
Michael Baker <michaelbaker4@my.whccd.edu>
2025-02-26 21:45:56 -0800

    Circle class done. Rectangle class created.

---
+++ COMP003A.LectureActivity6-2/Circle.cs
@@ -0,0 +1,29 @@
+﻿namespace COMP003A.LectureActivity6_2
+{
+    /// <summary>
+    /// Represents a circle with a specific area calculation.
+    /// </summary>
+    internal class Circle : Shape
+    {
+        // Auto-implemented property for the radius of the circle.
+        public double Radius { get; set; }
+
+        /// <summary>
+        /// Constructor for the Circle class.
+        /// </summary>
+        /// <param name="radius"></param>
+        
+        public Circle(double radius) 
+        {
+            Radius = radius;
+        }
+
+        /// <summary>
+        /// Displays the area of a circle
+        /// </summary>
+        public override void DisplayArea()
+        {
+            Console.WriteLine($"Circle Area: {Math.PI * Radius * Radius}");
+        }
+    }
+}
---
+++ COMP003A.LectureActivity6-2/Shape.cs
@@ -0,0 +1,18 @@
+﻿namespace COMP003A.LectureActivity6_2
+{
+    /// <summary>
+    /// Represents a base class for geometric shapes.
+    /// </summary>
+    internal class Shape
+    {
+        /// <summary>
+        /// Displays the area of a shape.
+        /// </summary>
+        
+        public virtual void DisplayArea()
+        {
+            Console.WriteLine("Calculating area of a shape.");
+        }
+
+    }
+}
